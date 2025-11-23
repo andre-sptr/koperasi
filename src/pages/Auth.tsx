aa5c3c3861d90914af8276c6136555640c0696d8
Andre Saputra <andresaputra07012019@gmail.com>
2025-11-23 23:38:56 +0700

    update

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -15,7 +15,7 @@ import { ShoppingBag } from "lucide-react";
 
 const loginSchema = z.object({
   email: z.string().email("Email tidak valid"),
-  password: z.string().min(6, "Password minimal 6 karakter"),
+  password: z.string().min(6, "Password minimal 8 karakter"),
 });
 
 const signupSchema = loginSchema.extend({
